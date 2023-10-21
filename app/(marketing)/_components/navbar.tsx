3b689287db3380da8d8297e42d18cfe23b39ed47
Rati <rziraqashvili@gmail.com>
2023-10-21 16:57:38 +0400

    Setup Auth

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -4,9 +4,15 @@ import { ModeToggle } from "@/components/mode-toggle";
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
+import { useConvexAuth } from "convex/react"
+import { Button } from "@/components/ui/button";
+import { SignInButton, UserButton } from "@clerk/clerk-react"
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 
 export const Navbar = () => {
   const scrolled = useScrollTop();
+  const {isAuthenticated, isLoading} = useConvexAuth();
 
   return (
     <div
@@ -17,6 +23,33 @@ export const Navbar = () => {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && (
+          <Spinner />
+        )}
+        {!isAuthenticated && !isLoading && (
+          <>
+          <SignInButton mode="modal">
+            <Button variant="ghost" size="sm">
+                Log in
+            </Button>
+          </SignInButton>
+          <SignInButton mode="modal">
+            <Button size="sm">
+                Get Jotion free
+            </Button>
+          </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+                <Link href="/documents">
+                  Enter Jotion
+                </Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: "https://uncommon-teal-39.clerk.accounts.dev",
+            applicationID: "convex"
+        }
+    ]
+}
