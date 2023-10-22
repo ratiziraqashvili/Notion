50d6e56ffc00c5084a240da103a45e46fbb4cfc9
Rati <rziraqashvili@gmail.com>
2023-10-22 13:01:46 +0400

    Added sidebar

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,7 +3,8 @@
 import { Spinner } from "@/components/spinner";
 import { useConvexAuth } from "convex/react";
 import { redirect } from "next/navigation";
-import Navigation from "./_components/navigation";
+import { Navigation } from "./_components/navigation";
+
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   const { isAuthenticated, isLoading } = useConvexAuth();
