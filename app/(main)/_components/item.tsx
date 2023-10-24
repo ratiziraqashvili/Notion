e556d79ad921867887a193cacf0d6282eeeaa7c2
Rati <rziraqashvili@gmail.com>
2023-10-24 16:00:38 +0400

    Achive functionality

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -53,6 +53,19 @@ export const Item = ({
   const { user } = useUser();
   const create = useMutation(api.documents.create);
   const router = useRouter();
+  const archive = useMutation(api.documents.archive);
+
+  const onArchive = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    event.stopPropagation();
+    if(!id) return;
+    const promise = archive({ id })
+
+    toast.promise(promise, {
+      loading: "Moving to trash...",
+      success: "Note moved to trash!",
+      error: "Failed to archive note.",
+    })
+  }
 
   const handleExpand = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
@@ -130,7 +143,7 @@ export const Item = ({
               side="right"
               forceMount
             >
-              <DropdownMenuItem onClick={() => {}}>
+              <DropdownMenuItem onClick={onArchive}>
                 <Trash className="h-4 2-4 mr-2" />
                 Delete
               </DropdownMenuItem>
