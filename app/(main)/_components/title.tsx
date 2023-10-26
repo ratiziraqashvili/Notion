16e27922686eab5f35a55d9907e278fd757939d0
Rati <rziraqashvili@gmail.com>
2023-10-26 16:43:46 +0400

    Finished Navbar

--- app/(main)/_components/title.tsx
+++ app/(main)/_components/title.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { Doc } from "@/convex/_generated/dataModel";
 import { useMutation } from "convex/react";
@@ -49,7 +50,15 @@ export const Title = ({ initialData }: TitleProps) => {
     <div className="flex items-center gap-x-1">
       {!!initialData.icon && <p>{initialData.icon}</p>}
       {isEditing ? (
-        <Input ref={inputRef} onClick={enableInput} onBlur={disableInput} onChange={onChange} onKeyDown={onKeyDown} value={title} className="h-7 px-2 focus-visible:ring-transparent" />
+        <Input
+          ref={inputRef}
+          onClick={enableInput}
+          onBlur={disableInput}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+          value={title}
+          className="h-7 px-2 focus-visible:ring-transparent"
+        />
       ) : (
         <Button
           onClick={enableInput}
@@ -62,4 +71,8 @@ export const Title = ({ initialData }: TitleProps) => {
       )}
     </div>
   );
+};
+
+Title.Skeleton = function TitleSkeleton() {
+  return <Skeleton className="h-7 w-20 rounded-md" />;
 };
