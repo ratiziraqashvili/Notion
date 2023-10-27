0df1fb076c8dac66aaadb66839d22b409b73dcda
Rati <rziraqashvili@gmail.com>
2023-10-27 22:06:46 +0400

    Added editor (Blocknote library)

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Cover } from "@/components/cover";
+import { Editor } from "@/components/editor";
 import { Toolbar } from "@/components/toolbar";
 import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
@@ -43,6 +44,7 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
       <Cover url={document.coverImage} />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar initialData={document} />
+        <Editor onChange={() => {}} initialContent={document.content} />
       </div>
     </div>
   );
